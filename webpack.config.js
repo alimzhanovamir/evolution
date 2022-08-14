b7fa646faacec72c26be701ae4b524e2d6e26b63
Amir Alimzhanov <aalimzhano@wiley.com>
2022-08-14 18:36:11 +0600

    init config

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const { ProvidePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: path.join(__dirname, 'src', 'index.tsx')
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 8080,
+    compress: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  output: {
+    filename: 'application.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'static', 'index.html'),
+        inject: false,
+    }),
+    new MiniCssExtractPlugin(),
+    new ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ]
+}
