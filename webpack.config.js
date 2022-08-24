e74847d3bc76a3c14571191d35779adea02a0846
Amir Alimzhanov <aalimzhano@wiley.com>
2022-08-24 23:18:53 +0600

    fix: Fixed for eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 const path = require('path');
-const { ProvidePlugin } = require('webpack')
+const { ProvidePlugin } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const devMode = process.env.NODE_ENV !== "production";
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: 'development',
   entry: {
-    app: path.join(__dirname, 'src', 'index.tsx')
+    app: path.join(__dirname, 'src', 'index.tsx'),
   },
   target: 'web',
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)?$/,
         use: 'ts-loader',
-        exclude: '/node_modules/'
+        exclude: '/node_modules/',
       },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
-          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ],
       },
     ],
@@ -42,16 +42,16 @@ module.exports = {
   },
   output: {
     filename: 'application.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'static', 'index.html'),
-        inject: false,
+      template: path.join(__dirname, 'static', 'index.html'),
+      inject: false,
     }),
     new MiniCssExtractPlugin(),
     new ProvidePlugin({
       process: 'process/browser',
     }),
-  ]
-}
+  ],
+};
